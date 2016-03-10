1083952916773be0980e705449e41a2bae00d942
Najko Jahn <najko.jahn@uni-bielefeld.de>
2016-03-10 18:09:22 +0000

    defunct functions

---
+++ R/defunct.r
@@ -0,0 +1,93 @@
+
+search_publications <- function(...)
+{
+  .Defunct("epmc_search", "rebi", "Function removed,
+           you can get this info using epmc_search")
+}
+
+get_MESH <- function(...)
+{
+  .Defunct("epmc_details", "rebi", "Function removed,
+           you can get this info using epmc_details")
+}
+
+
+get_grants <- function(...)
+{
+  .Defunct("epmc_details", "rebi", "Function removed,
+           you can get this info using epmc_details")
+}
+
+
+get_chemicals <- function(...)
+{
+  .Defunct("epmc_details", "rebi", "Function removed,
+           you can get this info using epmc_details")
+}
+
+
+get_author <- function(...)
+{
+  .Defunct("epmc_details", "rebi", "Function removed,
+           you can get this info using epmc_details")
+}
+
+
+get_comments <- function(...)
+{
+  .Defunct("epmc_details", "rebi", "Function removed,
+           you can get this info using epmc_details")
+}
+
+
+get_journal_info <- function(...)
+{
+  .Defunct("epmc_details", "rebi", "Function removed,
+           you can get this info using epmc_details")
+}
+
+
+get_fulltext <- function(...)
+{
+  .Defunct("epmc_details", "rebi", "Function removed,
+           you can get this info using epmc_details")
+}
+
+
+get_citations <- function(...)
+{
+  .Defunct("epmc_citations", "rebi", "Function removed,
+           you can get this info using epmc_citations")
+}
+
+
+get_references <- function(...)
+{
+  .Defunct("epmc_refs", "rebi", "Function removed,
+           you can get this info using epmc_refs")
+}
+
+
+lablinkxml <-function(...)
+{
+  .Defunct("lablinkxml", "rebi", "Function removed.")
+}
+
+
+parse_ftxt <-function(...)
+{
+  .Defunct("parse_ftxt", "rebi", "Function removed, use epmc_ftxt instead")
+}
+
+
+dblinkcount <-function(...)
+{
+  .Defunct("dblinkcount", "rebi", "Function removed, use epmc_db_count instead")
+}
+
+
+dblinkdata <-function(...)
+{
+  .Defunct("dblinkdata", "rebi", "Function removed, use epmc_db instead")
+}
+
---
+++ R/rebi-package.r
@@ -0,0 +1,24 @@
+#' rebi - R client for the Europe PMC RESTful article API
+#'
+#' What is rebi?
+#'
+#' rebi facilitates access to Europe PMC RESTful Web Service. Europe
+#' PMC covers life science literature and gives access to open access full
+#' texts. Coverage is not only restricted to Europe, but articles and
+#' abstracts are indexed from all over the world. As a partner in the PMC
+#' International (PMCi), Europe PMC ingests all PubMed content and extends its
+#' index with other sources, including Agricola, a bibliographic database of
+#' citations to the agricultural literature, or Biological Patents.
+#'
+#' Apart of searching abstracts and full text, rebi can be used to
+#' retrieve reference sections and citations, text-mined terms or cross-links
+#' to other databases hosted by the European Bioinformatics Institute (EBI).
+#'
+#' For information about Europe PMC, see their current paper:
+#' Europe PMC: a full-text literature database for the life sciences and
+#' platform for innovation. (2014). Nucleic Acids Research, 43(D1), D1042-D1048.
+#' \url{http://doi.org/10.1093/nar/gku1061}.
+#'
+#' @import plyr jsonlite httr xml2
+#' @name rebi-package
+NULL
