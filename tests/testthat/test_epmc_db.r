c15d9ce821c13920107936c718ba3c84c2778b29
Najko Jahn <najko.jahn@gmail.com>
2016-12-04 14:36:52 +0100

    uniprot works again #10

--- tests/testthat/test_epmc_db.r
+++ tests/testthat/test_epmc_db.r
@@ -2,18 +2,18 @@ context("testing epmc_db")
 
 test_that("epmc_db returns", {
   skip_on_cran()
-#  a <- epmc_db("12368864", db = "uniprot", limit = 50)
+  a <- epmc_db("12368864", db = "uniprot", limit = 50)
   b <- epmc_db("25249410", db = "embl")
-#  c <- epmc_db("14756321", db = "uniprot")
+  c <- epmc_db("14756321", db = "uniprot")
 
   #correct dimensions and class
-#  expect_output(str(a), "data.frame")
+  expect_output(str(a), "data.frame")
   expect_output(str(b), "data.frame")
-#  expect_output(str(c), "data.frame")
+  expect_output(str(c), "data.frame")
 
-#  expect_is(attr(a, "hit_count"), "integer")
+  expect_is(attr(a, "hit_count"), "integer")
   expect_is(attr(b, "hit_count"), "integer")
-#  expect_is(attr(c, "hit_count"), "integer")
+  expect_is(attr(c, "hit_count"), "integer")
 
   # expect_equal(nrow(a), 50)
 
