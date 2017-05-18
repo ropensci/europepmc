0883b09d44d5bc2720330f3bbe4632fdb40eb1ad
Najko Jahn <najko.jahn@gmail.com>
2017-05-18 19:59:41 +0200

    filter by epmc data source #21

--- R/epmc_hits_trend.R
+++ R/epmc_hits_trend.R
@@ -6,6 +6,23 @@
 #'  terminology and the UniProt synonym list are queried, too. Disabled by
 #'  default.
 #' @param period a vector of years (numeric) over which to perform the search
+#' @param data_src character, data source, by default Pubmed/MedLine index  (\code{med})
+#'   will be searched.
+#'   The following three letter codes represent the sources, which are currently
+#'   supported
+#'   \describe{
+#'     \item{agr}{Agricola is a bibliographic database of citations to the
+#'     agricultural literature created by the US National Agricultural Library
+#'     and its co-operators.}
+#'     \item{cba}{Chinese Biological Abstracts}
+#'     \item{ctx}{CiteXplore}
+#'     \item{eth}{EthOs Theses, i.e. PhD theses (British Library)}
+#'     \item{hir}{NHS Evidence}
+#'     \item{med}{PubMed/Medline NLM}
+#'     \item{nbk}{Europe PMC Book metadata}
+#'     \item{pat}{Biological Patents}
+#'     \item{pmc}{PubMed Central}
+#'     }
 #'
 #' @details A similar function was used in
 #'  \url{http://www.masalmon.eu/2017/05/14/evergreenreviewgraph/} where
@@ -22,43 +39,68 @@
 #' epmc_hits_trend('aspirin', period = 2006:2016, synonym = FALSE)
 #' # link to cran packages in reference lists
 #' epmc_hits_trend('REF:"cran.r-project.org*"', period = 2006:2016, synonym = FALSE)
-#' # more complex with source Medline and publication type review
-#' epmc_hits_trend('(REF:"cran.r-project.org*") AND (SRC:"MED") AND (PUB_TYPE:"Review" OR PUB_TYPE:"review-article")',
-#' period = 2006:2016, synonym = FALSE)
+#' # more complex with publication type review
+#' epmc_hits_trend('(REF:"cran.r-project.org*") AND (PUB_TYPE:"Review" OR PUB_TYPE:"review-article")', period = 2006:2016, synonym = FALSE)
 #' }
 epmc_hits_trend <- function(query,
                             synonym = TRUE,
+                            data_src = "med",
                             period = 1975:2016) {
   # input validation
-  stopifnot(is.character(query), is.logical(synonym), is.numeric(period))
+  stopifnot(is.character(query),
+            is.logical(synonym),
+            is.numeric(period))
+  if (!tolower(data_src) %in% supported_data_src)
+    stop(
+      paste0(
+        "Data source '",
+        data_src,
+        "' not supported. Try one of the
+        following sources: ",
+        paste0(supported_data_src, collapse = ", ")
+      )
+    )
   years <- period
-  results <- lapply(years,
-                    epmc_hits_trend_by_year,
-                    query = query,
-                    synonym = synonym)
+  results <- lapply(
+    years,
+    epmc_hits_trend_by_year,
+    query = query,
+    synonym = synonym,
+    data_src = data_src
+  )
   dplyr::bind_rows(results)
 }
 
-epmc_hits_trend_by_year <- function(year, query, synonym) {
-  queryforall <-
-    paste0('(FIRST_PDATE:[', year, '-01-01+TO+', year, '-12-31])')
-  all_hits <-
-    as.numeric(europepmc::epmc_profile(queryforall, synonym)$pubType[1, 2])
+epmc_hits_trend_by_year <-
+  function(year, query, synonym, data_src) {
+    queryforall <-
+      paste0('(FIRST_PDATE:[',
+             year,
+             '-01-01+TO+',
+             year,
+             '-12-31]) AND SRC:',
+             data_src)
+    all_hits <-
+      as.numeric(europepmc::epmc_profile(queryforall, synonym)$pubType[1, 2])
 
-  queryforterm <-
-    paste0('(FIRST_PDATE:[',
-           year,
-           '-01-01+TO+',
-           year,
-           '-12-31]) AND ',
-           query)
+    queryforterm <-
+      paste0(
+        '(FIRST_PDATE:[',
+        year,
+        '-01-01+TO+',
+        year,
+        '-12-31]) AND ',
+        query,
+        ' AND SRC:',
+        data_src
+      )
 
-  query_hits <-
-    as.numeric(europepmc::epmc_profile(queryforterm, synonym)$pubType[1, 2])
+    query_hits <-
+      as.numeric(europepmc::epmc_profile(queryforterm, synonym)$pubType[1, 2])
 
-  return(dplyr::data_frame(
-    year = year,
-    all_hits = all_hits,
-    query_hits = query_hits
-  ))
-}
+    return(dplyr::data_frame(
+      year = year,
+      all_hits = all_hits,
+      query_hits = query_hits
+    ))
+  }
--- tests/testthat/test_epmc_hits_trend.R
+++ tests/testthat/test_epmc_hits_trend.R
@@ -4,19 +4,26 @@ test_that("epmc_hits_trend returns", {
   skip_on_cran()
   a <- epmc_hits_trend('aspirin', period = 2006:2016, synonym = FALSE)
   b <- epmc_hits_trend('REF:"cran.r-project.org*"', period = 2006:2016, synonym = FALSE)
-  c <- epmc_hits_trend('(REF:"cran.r-project.org*") AND (SRC:"MED") AND (PUB_TYPE:"Review" OR PUB_TYPE:"review-article")', period = 2006:2016, synonym = FALSE)
+  c <- epmc_hits_trend('(REF:"cran.r-project.org*") AND (PUB_TYPE:"Review" OR PUB_TYPE:"review-article")', period = 2006:2016, synonym = FALSE)
+  d <- epmc_hits_trend('aspirin', period = 2006:2016, data_src = "med")
+
+
 
   expect_is(a, "tbl_df")
   expect_is(b, "tbl_df")
   expect_is(c, "tbl_df")
+  expect_is(d, "tbl_df")
 
   expect_true(all(names(a) == c("year", "all_hits", "query_hits")))
   expect_true(all(names(b) == c("year", "all_hits", "query_hits")))
   expect_true(all(names(c) == c("year", "all_hits", "query_hits")))
+  expect_true(all(names(d) == c("year", "all_hits", "query_hits")))
+
 
   expect_true(all(a$year == 2006:2016))
   expect_true(all(b$year == 2006:2016))
   expect_true(all(c$year == 2006:2016))
+  expect_true(all(d$year == 2006:2016))
 })
 
 test_that("epmc_hits_trend fails correctly", {
@@ -25,4 +32,12 @@ test_that("epmc_hits_trend fails correctly", {
   expect_error(epmc_hits_trend("aspirin", period = "2006:2004", synonym = FALSE))
   expect_error(epmc_hits_trend("aspirin", period = 2006:2008, synonym = "f"))
   expect_error(epmc_hits_trend(query = 123, period = 2006:2008))
+  expect_error(epmc_hits_trend("aspirin", data_src = "abc"))
+})
+
+test_that("epmc_hits_trend synonym search works correctly", {
+  a <- epmc_hits_trend('malaria', period = 2012:2016, synonym = FALSE)
+  b <- epmc_hits_trend('malaria', period = 2012:2016, synonym = TRUE)
+
+  expect_gt(sum(b$query_hits), sum(a$query_hits))
 })
