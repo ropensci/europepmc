be11b0759b76d21ccd5578f13ff569060598044c
Najko Jahn <najko.jahn@gmail.com>
2017-09-08 14:21:26 +0200

    fix test

--- tests/testthat/test_epmc_lablinks.r
+++ tests/testthat/test_epmc_lablinks.r
@@ -2,7 +2,7 @@ context("testing epmc_lablinks")
 
 test_that("epmc_lablinks returns", {
   skip_on_cran()
-  a <- epmc_lablinks("25389392", lab_id = "1562")
+  a <- epmc_lablinks("26345659", lab_id = "1056")
   b <- epmc_lablinks("24007304", lab_id = "1507")
   c <- epmc_lablinks("12736239", lab_id = "1056")
 
