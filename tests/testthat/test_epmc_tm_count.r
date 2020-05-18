17d23c3cb85190eef3c4a956bc6d2db009cf343b
Najko Jahn <najko.jahn@gmail.com>
2020-05-18 17:29:38 +0200

    defunc deprecated API text mining methods

--- tests/testthat/test_epmc_tm_count.r
+++ tests/testthat/test_epmc_tm_count.r
@@ -2,18 +2,5 @@ context("testing epmc_tm_count")
 
 test_that("epmc_tm_count returns", {
   skip_on_cran()
-  a <- epmc_tm_count("25249410")
-  b <- epmc_tm_count("PMC4340542", data_src = "pmc")
-  c <- epmc_tm_count("PMC4340542", data_src = "pmc")
-
-  #correct dimensions and class
-  expect_output(str(a), "data.frame")
-  expect_output(str(b), "data.frame")
-  expect_output(str(c), "data.frame")
-
-  # fails correctly
-  expect_message(epmc_tm_count("14756321"),
-               "Sorry, no text-mined terms found")
-  expect_null(epmc_tm_count("14756321"))
-  expect_error(epmc_tm_count("13814508", data_src = "abc"))
+  expect_error(epmc_tm_count("25249410"))
 })
