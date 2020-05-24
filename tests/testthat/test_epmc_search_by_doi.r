50cdaf2adb4b75814e1c6727faec65df66acf9ab
Najko Jahn <najko.jahn@gmail.com>
2020-05-24 17:47:59 +0200

    new fxn searching epmc by doi names

---
+++ tests/testthat/test_epmc_search_by_doi.r
@@ -0,0 +1,22 @@
+context("testing epmc_search_by_doi")
+
+test_that("epmc_search_by_doi returns", {
+  a <- epmc_search_by_doi(doi = "10.1161/strokeaha.117.018077")
+  my_dois <- c(
+    "10.1159/000479962",
+    "10.1002/sctm.17-0081",
+    "10.1161/strokeaha.117.018077",
+    "10.1007/s12017-017-8447-9"
+  )
+  b <- epmc_search_by_doi(doi = my_dois)
+  c <- epmc_search_by_doi(doi = "10.1161/strokeaha.117.018077", output = "raw")
+  d <- epmc_search_by_doi(doi = my_dois, output = "id_list")
+
+  expect_is(a, "tbl_df")
+  expect_is(b, "tbl_df")
+  expect_is(c, "list")
+  expect_is(d, "tbl_df")
+
+  expect_error(epmc_search_by_doi(doi = my_dois, output = "kd"))
+  expect_error(epmc_search_by_doi(doi = my_dois, verbose = "no"))
+})
